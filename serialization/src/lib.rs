f8a346958f06b2b1bf43e1dc07362608522a8b85
koushiro <koushiro.cqx@gmail.com>
2019-03-01 15:03:36 +0800

    Finish serialization
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- serialization/src/lib.rs
+++ serialization/src/lib.rs
@@ -1,19 +1,15 @@
-extern crate byteorder;
-extern crate primitives;
-
 mod compact_integer;
 mod impls;
 mod list;
 mod reader;
 mod stream;
 
-pub use primitives::{hash, bytes, compact};
+pub use primitives::*;
 
 pub use compact_integer::CompactInteger;
 pub use list::List;
-pub use reader::{Reader, Deserializable, deserialize, deserialize_iterator, ReadIterator, Error};
+pub use reader::{deserialize, deserialize_iterator, Deserializable, Error, ReadIterator, Reader};
 pub use stream::{
-	Stream, Serializable, serialize, serialize_with_flags, serialize_list, serialized_list_size,
-	serialized_list_size_with_flags, SERIALIZE_TRANSACTION_WITNESS,
+    serialize, serialize_list, serialize_with_flags, serialized_list_size,
+    serialized_list_size_with_flags, Serializable, Stream, SERIALIZE_TRANSACTION_WITNESS,
 };
-
