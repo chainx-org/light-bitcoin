1c23d2f30dd0db655a6d12b5a7782d09c3912da9
koushiro <koushiro.cqx@gmail.com>
2019-03-02 16:00:51 +0800

    Add base58 supported no_std
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- keys/src/display.rs
+++ keys/src/display.rs
@@ -1,10 +1,13 @@
-use std::ops::Deref;
-use Error;
+use rstd::{ops, prelude::*};
+
+use super::error::Error;
 
 pub trait DisplayLayout {
-	type Target: Deref<Target = [u8]>;
+    type Target: ops::Deref<Target = [u8]>;
 
-	fn layout(&self) -> Self::Target;
+    fn layout(&self) -> Self::Target;
 
-	fn from_layout(data: &[u8]) -> Result<Self, Error> where Self: Sized;
+    fn from_layout(data: &[u8]) -> Result<Self, Error>
+    where
+        Self: Sized;
 }
