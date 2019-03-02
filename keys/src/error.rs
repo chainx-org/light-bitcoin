cdc210802ff0213a9758c7978a3c8b694f94c092
koushiro <koushiro.cqx@gmail.com>
2019-03-02 16:18:59 +0800

    Add no_std support for keys
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- keys/src/error.rs
+++ keys/src/error.rs
@@ -1,44 +1,44 @@
-use std::fmt;
-use secp256k1::Error as SecpError;
+use rstd::{fmt, prelude::*};
 
 #[derive(Debug, PartialEq)]
 pub enum Error {
-	InvalidPublic,
-	InvalidSecret,
-	InvalidMessage,
-	InvalidSignature,
-	InvalidNetwork,
-	InvalidChecksum,
-	InvalidPrivate,
-	InvalidAddress,
-	FailedKeyGeneration,
+    InvalidPublic,
+    InvalidSecret,
+    InvalidMessage,
+    InvalidSignature,
+    InvalidNetwork,
+    InvalidChecksum,
+    InvalidPrivate,
+    InvalidAddress,
+    FailedKeyGeneration,
 }
 
 impl fmt::Display for Error {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		let msg = match *self {
-			Error::InvalidPublic => "Invalid Public",
-			Error::InvalidSecret => "Invalid Secret",
-			Error::InvalidMessage => "Invalid Message",
-			Error::InvalidSignature => "Invalid Signature",
-			Error::InvalidNetwork => "Invalid Network",
-			Error::InvalidChecksum => "Invalid Checksum",
-			Error::InvalidPrivate => "Invalid Private",
-			Error::InvalidAddress => "Invalid Address",
-			Error::FailedKeyGeneration => "Key generation failed",
-		};
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let msg = match *self {
+            Error::InvalidPublic => "Invalid Public",
+            Error::InvalidSecret => "Invalid Secret",
+            Error::InvalidMessage => "Invalid Message",
+            Error::InvalidSignature => "Invalid Signature",
+            Error::InvalidNetwork => "Invalid Network",
+            Error::InvalidChecksum => "Invalid Checksum",
+            Error::InvalidPrivate => "Invalid Private",
+            Error::InvalidAddress => "Invalid Address",
+            Error::FailedKeyGeneration => "Key generation failed",
+        };
 
-		msg.fmt(f)
-	}
+        msg.fmt(f)
+    }
 }
 
-impl From<SecpError> for Error {
-	fn from(e: SecpError) -> Self {
-		match e {
-			SecpError::InvalidPublicKey	=> Error::InvalidPublic,
-			SecpError::InvalidSecretKey => Error::InvalidSecret,
-			SecpError::InvalidMessage => Error::InvalidMessage,
-			_ => Error::InvalidSignature,
-		}
-	}
+impl From<secp256k1::Error> for Error {
+    fn from(e: secp256k1::Error) -> Self {
+        match e {
+            secp256k1::Error::InvalidSignature => Error::InvalidSignature,
+            secp256k1::Error::InvalidPublicKey => Error::InvalidPublic,
+            secp256k1::Error::InvalidSecretKey => Error::InvalidSecret,
+            secp256k1::Error::InvalidMessage => Error::InvalidMessage,
+            _ => Error::InvalidSignature,
+        }
+    }
 }
--- keys/src/private.rs
+++ keys/src/private.rs
@@ -1,164 +1,179 @@
 //! Secret with additional network identifier and format type
 
-use std::fmt;
-use std::str::FromStr;
-use secp256k1::key;
-use secp256k1::Message as SecpMessage;
-use hex::ToHex;
-use base58::{ToBase58, FromBase58};
-use crypto::checksum;
-use hash::H520;
-use network::Network;
-use {Secret, DisplayLayout, Error, Message, Signature, CompactSignature, SECP256K1};
+use rstd::{fmt, prelude::*, str};
+
+use bitcrypto::checksum;
+use primitives::H520;
+
+use base58::{FromBase58, ToBase58};
+
+use super::address::Network;
+use super::display::DisplayLayout;
+use super::error::Error;
+use super::signature::{CompactSignature, Signature};
+use super::{Message, Secret};
 
 /// Secret with additional network identifier and format type
-#[derive(PartialEq)]
+#[derive(PartialEq, Clone, Default)]
 pub struct Private {
-	/// The network on which this key should be used.
-	pub network: Network,
-	/// ECDSA key.
-	pub secret: Secret,
-	/// True if this private key represents a compressed address.
-	pub compressed: bool,
+    /// The network on which this key should be used.
+    pub network: Network,
+    /// ECDSA key.
+    pub secret: Secret,
+    /// True if this private key represents a compressed address.
+    pub compressed: bool,
 }
 
 impl Private {
-	pub fn sign(&self, message: &Message) -> Result<Signature, Error> {
-		let context = &SECP256K1;
-		let secret = try!(key::SecretKey::from_slice(context, &*self.secret));
-		let message = try!(SecpMessage::from_slice(&**message));
-		let signature = try!(context.sign(&message, &secret));
-		let data = signature.serialize_der(context);
-		Ok(data.into())
-	}
+    pub fn sign(&self, message: &Message) -> Result<Signature, Error> {
+        let secret = secp256k1::SecretKey::parse(self.secret.as_fixed_bytes())?;
+        let message = secp256k1::Message::parse(message.as_fixed_bytes());
+        let (signature, _recovery_id) = secp256k1::sign(&message, &secret)?;
+        Ok(signature.serialize_der().as_ref().to_vec().into())
+    }
 
-	pub fn sign_compact(&self, message: &Message) -> Result<CompactSignature, Error> {
-		let context = &SECP256K1;
-		let secret = try!(key::SecretKey::from_slice(context, &*self.secret));
-		let message = try!(SecpMessage::from_slice(&**message));
-		let signature = try!(context.sign_recoverable(&message, &secret));
-		let (recovery_id, data) = signature.serialize_compact(context);
-		let recovery_id = recovery_id.to_i32() as u8;
-		let mut signature = H520::default();
-		signature[1..65].copy_from_slice(&data[0..64]);
-		if self.compressed {
-			signature[0] = 27 + recovery_id + 4;
-		} else {
-			signature[0] = 27 + recovery_id;
-		}
-		Ok(signature.into())
-	}
+    pub fn sign_compact(&self, message: &Message) -> Result<CompactSignature, Error> {
+        let secret = secp256k1::SecretKey::parse(self.secret.as_fixed_bytes())?;
+        let message = secp256k1::Message::parse(message.as_fixed_bytes());
+        let (signature, recovery_id) = secp256k1::sign(&message, &secret)?;
+        let recovery_id = recovery_id.serialize();
+        let data = signature.serialize();
+
+        let mut compact_signature = [0u8; 65];
+        compact_signature[0] = if self.compressed {
+            27 + recovery_id + 4
+        } else {
+            27 + recovery_id
+        };
+        compact_signature[1..65].copy_from_slice(&data[0..64]);
+        Ok(H520::from_slice(&compact_signature).into())
+    }
 }
 
 impl DisplayLayout for Private {
-	type Target = Vec<u8>;
+    type Target = Vec<u8>;
 
-	fn layout(&self) -> Self::Target {
-		let mut result = vec![];
-		let network_byte = match self.network {
-			Network::Mainnet => 128,
-			Network::Testnet => 239,
-		};
+    fn layout(&self) -> Self::Target {
+        let mut result = vec![];
+        let network_byte = match self.network {
+            Network::Mainnet => 128,
+            Network::Testnet => 239,
+        };
 
-		result.push(network_byte);
-		result.extend(&*self.secret);
-		if self.compressed {
-			result.push(1);
-		}
-		let cs = checksum(&result);
-		result.extend_from_slice(&*cs);
-		result
-	}
+        result.push(network_byte);
+        result.extend(self.secret.as_bytes());
+        if self.compressed {
+            result.push(1);
+        }
+        let cs = checksum(&result);
+        result.extend_from_slice(cs.as_bytes());
+        result
+    }
 
-	fn from_layout(data: &[u8]) -> Result<Self, Error> where Self: Sized {
-		let compressed = match data.len() {
-			37 => false,
-			38 => true,
-			_ => return Err(Error::InvalidPrivate),
-		};
+    fn from_layout(data: &[u8]) -> Result<Self, Error>
+    where
+        Self: Sized,
+    {
+        let compressed = match data.len() {
+            37 => false,
+            38 => true,
+            _ => return Err(Error::InvalidPrivate),
+        };
 
-		if compressed && data[data.len() - 5] != 1 {
-			return Err(Error::InvalidPrivate);
-		}
+        if compressed && data[data.len() - 5] != 1 {
+            return Err(Error::InvalidPrivate);
+        }
 
-		let cs = checksum(&data[0..data.len() - 4]);
-		if &data[data.len() - 4..] != &*cs {
-			return Err(Error::InvalidChecksum);
-		}
+        let cs = checksum(&data[0..data.len() - 4]);
+        if &data[data.len() - 4..] != cs.as_bytes() {
+            return Err(Error::InvalidChecksum);
+        }
 
-		let network = match data[0] {
-			128 => Network::Mainnet,
-			239 => Network::Testnet,
-			_ => return Err(Error::InvalidPrivate),
-		};
+        let network = match data[0] {
+            128 => Network::Mainnet,
+            239 => Network::Testnet,
+            _ => return Err(Error::InvalidPrivate),
+        };
 
-		let mut secret = Secret::default();
-		secret.copy_from_slice(&data[1..33]);
+        let secret = Secret::from_slice(&data[1..33]);
 
-		let private = Private {
-			network: network,
-			secret: secret,
-			compressed: compressed,
-		};
+        let private = Private {
+            network,
+            secret,
+            compressed,
+        };
 
-		Ok(private)
-	}
+        Ok(private)
+    }
 }
 
 impl fmt::Debug for Private {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		try!(writeln!(f, "network: {:?}", self.network));
-		try!(writeln!(f, "secret: {}", self.secret.to_hex::<String>()));
-		writeln!(f, "compressed: {}", self.compressed)
-	}
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(f, "network: {:?}", self.network)?;
+        writeln!(f, "secret: {}", self.secret)?;
+        writeln!(f, "compressed: {}", self.compressed)
+    }
 }
 
 impl fmt::Display for Private {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		self.layout().to_base58().fmt(f)
-	}
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.layout().to_base58().fmt(f)
+    }
 }
 
-impl FromStr for Private {
-	type Err = Error;
+impl str::FromStr for Private {
+    type Err = Error;
 
-	fn from_str(s: &str) -> Result<Self, Error> where Self: Sized {
-		let hex = try!(s.from_base58().map_err(|_| Error::InvalidPrivate));
-		Private::from_layout(&hex)
-	}
+    fn from_str(s: &str) -> Result<Self, Error>
+    where
+        Self: Sized,
+    {
+        let hex = s.from_base58().map_err(|_| Error::InvalidPrivate)?;
+        Private::from_layout(&hex)
+    }
 }
 
 impl From<&'static str> for Private {
-	fn from(s: &'static str) -> Self {
-		s.parse().unwrap()
-	}
+    fn from(s: &'static str) -> Self {
+        s.parse().unwrap()
+    }
 }
 
 #[cfg(test)]
 mod tests {
-	use hash::H256;
-	use network::Network;
-	use super::Private;
+    use super::*;
+    use primitives::H256;
+    use rustc_hex::FromHex;
 
-	#[test]
-	fn test_private_to_string() {
-		let private = Private {
-			network: Network::Mainnet,
-			secret: H256::from_reversed_str("063377054c25f98bc538ac8dd2cf9064dd5d253a725ece0628a34e2f84803bd5"),
-			compressed: false,
-		};
+    #[test]
+    fn test_private_to_string() {
+        let mut secret: Vec<u8> = FromHex::from_hex("063377054c25f98bc538ac8dd2cf9064dd5d253a725ece0628a34e2f84803bd5").unwrap();
+        secret.reverse();
+        let private = Private {
+            network: Network::Mainnet,
+            secret: H256::from_slice(&secret),
+            compressed: false,
+        };
 
-		assert_eq!("5KSCKP8NUyBZPCCQusxRwgmz9sfvJQEgbGukmmHepWw5Bzp95mu".to_owned(), private.to_string());
-	}
+        assert_eq!(
+            "5KSCKP8NUyBZPCCQusxRwgmz9sfvJQEgbGukmmHepWw5Bzp95mu".to_owned(),
+            private.to_string()
+        );
+    }
 
-	#[test]
-	fn test_private_from_str() {
-		let private = Private {
-			network: Network::Mainnet,
-			secret: H256::from_reversed_str("063377054c25f98bc538ac8dd2cf9064dd5d253a725ece0628a34e2f84803bd5"),
-			compressed: false,
-		};
+    #[test]
+    fn test_private_from_str() {
+        let mut secret: Vec<u8> = FromHex::from_hex("063377054c25f98bc538ac8dd2cf9064dd5d253a725ece0628a34e2f84803bd5").unwrap();
+        secret.reverse();
+        let private = Private {
+            network: Network::Mainnet,
+            secret: H256::from_slice(&secret),
+            compressed: false,
+        };
 
-		assert_eq!(private, "5KSCKP8NUyBZPCCQusxRwgmz9sfvJQEgbGukmmHepWw5Bzp95mu".into());
-	}
+        assert_eq!(
+            private,
+            "5KSCKP8NUyBZPCCQusxRwgmz9sfvJQEgbGukmmHepWw5Bzp95mu".into()
+        );
+    }
 }
