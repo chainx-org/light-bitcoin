5231f63cad63e03ec4f948a92f6ac555a3026d8f
koushiro <koushiro.cqx@gmail.com>
2019-03-21 13:59:16 +0800

    Remove useless comment in primitives
    Signed-off-by: koushiro <koushiro.cqx@gmail.com>

--- primitives/src/lib.rs
+++ primitives/src/lib.rs
@@ -26,25 +26,3 @@ construct_fixed_hash! {
     /// Fixed-size uninterpreted hash type with 65 bytes (520 bits) size.
     pub struct H520(65);
 }
-
-/*
-#[cfg(feature = "impl-serde")]
-mod serde {
-    use super::*;
-    impl_fixed_hash_serde!(H32, 4);
-    impl_fixed_hash_serde!(H48, 6);
-    impl_fixed_hash_serde!(H264, 33);
-    impl_fixed_hash_serde!(H520, 65);
-}
-*/
-
-/*
-#[cfg(feature = "impl-codec")]
-mod codec {
-    use super::*;
-    impl_fixed_hash_codec!(H32, 4);
-    impl_fixed_hash_codec!(H48, 6);
-    impl_fixed_hash_codec!(H264, 33);
-    impl_fixed_hash_codec!(H520, 65);
-}
-*/
